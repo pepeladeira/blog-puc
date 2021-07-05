df4c1ae2e0476ecac541c2f681555ab34fcba0d6
Pedro Ladeira <pmladeira@live.com>
2021-07-05 01:50:01 -0300

    finish

--- src/component/BlogDetail/BlogDetail.js
+++ src/component/BlogDetail/BlogDetail.js
@@ -1,31 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import BlogComment from '../BlogComment/BlogComment';
-import { useParams } from 'react-router-dom';
-import Box from '@material-ui/core/Box';
-import './BlogDetail.css';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Box from "@material-ui/core/Box";
+import "./BlogDetail.css";
 
 const BlogDetail = () => {
-    const {postID} = useParams();
-    const postDetailUrl = `https://jsonplaceholder.typicode.com/posts/${postID}`;
+  const { postID } = useParams();
+  const postDetailUrl = `https://60e27aee5a5596001730f405.mockapi.io/api/blog/posts/${postID}`;
 
-    const [post, setPost] = useState({})
+  const [post, setPost] = useState({});
 
-    // adding post details using post id.
-    useEffect(() => {
-        fetch(postDetailUrl)
-        .then(res => res.json())
-        .then(data => setPost(data))
-    }, []);
+  // adding post details using post id.
+  useEffect(() => {
+    fetch(postDetailUrl)
+      .then((res) => res.json())
+      .then((data) => setPost(data));
+  }, []);
 
-    return (
-        <div className="post-container">
-            <Box p={4} boxShadow={2}>
-                <h1>{post.title}</h1>
-                <p>{post.body}</p>
-            </Box>
-            <BlogComment postid={postID}></BlogComment>
-        </div>
-    );
+  return (
+    <div className="post-container">
+      <Box p={4} boxShadow={2}>
+        <h1>{post.title}</h1>
+        <p>{post.body}</p>
+        <p>
+          Para se divertir com o projeto, você pode acessar por{" "}
+          <a href={post.projectLink} target="_blank" rel="noopener noreferrer">
+            esse link
+          </a>{" "}
+          ou assistir o vídeo abaixo:
+        </p>
+        <iframe
+          width="560"
+          height="315"
+          src={post.videoURl}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </Box>
+    </div>
+  );
 };
 
 export default BlogDetail;
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,31 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import Box from '@material-ui/core/Box';
-import './Home.css';
+import React, { useEffect, useState } from "react";
+import Box from "@material-ui/core/Box";
+import "./Home.css";
 import { Link } from "react-router-dom";
 
-
 const Home = () => {
-    const [posts, setPosts]= useState([])
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then (res => res.json())
-        .then (date => setPosts(date))
-    }, []);
+  const [posts, setPosts] = useState([]);
 
-    return (
-        <div className="main-container">    
-            {
-                posts.map(post => {
-                    return <Box component="span" display="block" p={2} m={3} bgcolor="background.paper" key={post.id}>
-                            <span>Topic: {post.id}</span>
-                            <h2>{post.title}</h2>
-                            <Link to={`/post/${post.id}`}><button>Read more..</button></Link>
-                            </Box>
-                })
-             }
-           
-        </div>
-    );
+  useEffect(() => {
+    fetch("https://60e27aee5a5596001730f405.mockapi.io/api/blog/posts")
+      .then((res) => res.json())
+      .then((date) => setPosts(date));
+  }, []);
+
+  return (
+    <div className="main-container">
+      {posts.map((post) => {
+        return (
+          <Box
+            component="span"
+            display="block"
+            p={2}
+            m={3}
+            bgcolor="background.paper"
+            key={post.id}
+          >
+            <h2>{post.title}</h2>
+            <Link to={`/post/${post.id}`}>
+              <button>Ler mais...</button>
+            </Link>
+          </Box>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Home;
